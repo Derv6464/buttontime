372fb85cdcc0f54db1a982212d1c1342a9ee98f5
Derv6464 <36761419+Derv6464@users.noreply.github.com>
2020-12-09 11:06:09 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,10 @@
+let tTotal = 0
 let Start_time = 0
 let End_time = 0
-let tTotal = 0
+input.onButtonPressed(Button.A, function () {
+    buttonTime()
+    basic.showNumber(tTotal / 1000)
+})
 function buttonTime () {
     if (pins.digitalReadPin(DigitalPin.P0) == 1) {
         Start_time = input.runningTime()
